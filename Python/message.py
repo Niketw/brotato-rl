297fcf98283a36d73fccd08d4975556cab99a5ff
Aniket Inamdar <asinamdar2024@gmail.com>
2025-06-18 00:38:39 +0400

    hm

---
+++ Python/message.py
@@ -0,0 +1,9 @@
+from tkinter import *
+
+root = Tk()
+root.title("message")
+root.geometry("300x100")
+
+label = Label(text="change windows display scaling if screen too small").pack()
+
+root.mainloop()
